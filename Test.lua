fa5b12975c901017932c5ff8f4034282fe879cb2
UnstableSolutions <162848579+UnstableSolutions@users.noreply.github.com>
2024-06-07 23:00:11 -0500

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,29 +1,22 @@
-
 local function isValidKey(key)
     
-    local codeURL = "https://raw.githubusercontent.com/UnstableSolutions/Whitelist-sys/main/Keys.lua"
-    
-    
-    local success, code = pcall(game.HttpService.GetAsync, game.HttpService, codeURL)
-    
-   
-    if success then
-       
-        local whitelist = loadstring(code)()
+    local whitelist = {
         
+        "jk21p9vUObsn",
+        "valid_key_2",
         
-        for _, whitelistKey in ipairs(whitelist) do
-            if whitelistKey == key then
-                return true 
-            end
+    }
+    
+    
+    for _, whitelistKey in ipairs(whitelist) do
+        if whitelistKey == key then
+            return true 
         end
-    else
-     
-        print("Error fetching whitelist Lua code:", code)
     end
     
     return false 
 end
+
 
 if _G.Key and isValidKey(_G.Key) then
     print("Key is valid. Access granted.")
